50fdd0c424679aaf112d0d3b353a541e7a1a6003
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-08-12 02:56:20 +0600

    feat: add middleware module

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
+const middleware = require('./utils/middleware');
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -17,6 +18,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.json());
 
+app.use(middleware.requestLogger);
+
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
