9ca1323bc655f6def5f88e77a2e92146ade4ce49
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-08-12 02:46:12 +0600

    refactor: add app.js module

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const config = require('./utils/config');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const blogsRouter = require('./controllers/blogs');
-const logger = require('./utils/logger');
-const mongoose = require('mongoose');
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('Connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('Failed connect to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
+const app = require('./app');
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
