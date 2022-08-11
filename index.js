377020f093db7511188c9f24a228f70d5f7d5ef4
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-08-12 02:42:04 +0600

    feat: update index.js

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const config = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
 const mongoose = require('mongoose');
-// const Blog = require('./models/blog');
 
-mongoose.connect(config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('Failed connect to MongoDB:', error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
